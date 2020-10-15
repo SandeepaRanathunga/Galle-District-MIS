da2991b702d8b9f1eb98096a62bca2123e204fce
Sandeepa Ranathunga <sandeeparanatungavcg@gmail.com>
2020-10-16 02:11:08 +0530

    final footer created

--- includes/footer.php
+++ includes/footer.php
@@ -1,39 +1,30 @@
 <footer>
-
-        <div class="footerbox blue"> 
-        <div class="flex-container" >
-                        <div style="width:25%"><h2><br>Related Links</h2></div>
-                        <div style="width:4%"><img src="./images/logo.png" height="60px"></div>
-                        <div style="width:30%">
-                                <h2>Galle District Secretariat
-                                        <br>
-                                        <a href="http://www.galle.dist.gov.lk/">galle.dist.gov.lk/</a>
-                                </h2>
-                        </div>
-                        <div style="width:4%"><img src="./images/logo.png" height="60px"></div>
-                        <div style="width:30%">
-                                <h2>Galle District Secretariat
-                                        <br>
-                                        <a href="http://www.galle.dist.gov.lk/">galle.dist.gov.lk/</a>
-                                </h2>
-                        </div>
-
-                </div>     
-        </div>
-
-        <div class="footerbox" > 
-                <h2 class="footertextalignleft" style="width: 80%">Copyright © 2020 Galle District Secretariat. All Rights Reserved.
-                <br>
-                Developed in Association with <font color="yellow"> University of Colombo School of Computing </font>
+    <div class="footerbox"> 
+        <div class="blue" >
+            <div style="flex:1">
+                    <h2>Related Links</h2>
+            </div>
+            <div class="link_container">
+                <img src="./images/logo.png">
+                <div style="">
+                        <h2>Galle District Secretariat</h2>
+                        <a href="http://www.galle.dist.gov.lk/">www.galle.dist.gov.lk</a>
+                </div>        
+            </div>
+            <div class="link_container">
+                <img src="./images/logo.png">
+                <div>
+                        <h2>Ministry of Home Affairs</h2>
+                        <a href="http://www.moha.gov.lk/">www.moha.gov.lk</a> 
+                </div>
+            </div>
+        </div>     
+        <div class="black" > 
+                <h2 >
+                Copyright © 2020 Galle District Secretariat. All Rights Reserved.<br>Developed in Association with <span> University of Colombo School of Computing</span>
                 </h2>
-                <img class="footertextalignright" height="50px" src="./images/footer_gif.gif"> </h2>
-                
-                 
+                <img src="./images/footer_gif.gif">  
         </div>
-        
-        
-        
-         
-          
+    </div>
 </footer>
 <script src="assets/navigation.js"></script>
