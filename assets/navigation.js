b5098845ff0b28c49f603af513b1287c0b8cf0f5
Sandeepa Ranathunga <sandeeparanatungavcg@gmail.com>
2020-11-12 02:12:40 +0530

    mvc branch cleaned

--- assets/navigation.js
+++
@@ -1,8 +0,0 @@
-function addResponsiveness() {
-    var tag = document.getElementById("navbar");
-    if (tag.className === "topnav clearfix") {
-      tag.className += " responsive";
-    } else {
-      tag.className = "topnav clearfix";
-    }
-  }
