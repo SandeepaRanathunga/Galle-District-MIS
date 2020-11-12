91ed2d5626c89b361d018b25ad6a772aba6eaf58
sasinduly <2018is045@stu.ucsc.cmb.ac.lk>
2020-11-12 19:13:15 +0530

    font updated not working

--- div_home.php
+++ div_home.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin - Home </title>
+    <title>Division - Home </title>
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/userHome.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" type="text/css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu">
     <link rel="icon" href="./images/logo.png">
     <style>
         body{
@@ -17,8 +17,6 @@
 
 <body>
     <?php include 'includes/divHeader.php';?>
-    <!-- ?php include 'includes/guestNav.php';?--->
-    <!-- COPOIED CODE--> 
     <div class="button-flex-container" >                  
         <div> <a href="#">Notifications</a></div>
         <div> <a href="#">Respond to contractor requests</a></div>
@@ -29,6 +27,6 @@
         <div> <a href="#">Launch Project</a></div>
     </div>     
     <!-- COPOIED CODE-->
-
+    <?php require_once('includes/footer.php');?>
 </body>
 </html>
