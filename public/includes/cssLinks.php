16770c8b391737e25d3162eba167d0a48953f988
sasinduly <2018is045@stu.ucsc.cmb.ac.lk>
2020-11-20 02:42:15 +0530

    dis manage funds ui updated

--- public/includes/cssLinks.php
+++ public/includes/cssLinks.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="css/header.css">
 <link rel="stylesheet" href="css/common.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" type="text/css">
+
