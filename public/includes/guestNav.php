3bbecb6230782b3adb3331aa0ae82f3766de66ea
Sandeepa Ranathunga <sandeeparanatungavcg@gmail.com>
2020-11-22 15:04:57 +0530

    home contractor tab view done

--- public/includes/guestNav.php
+++ public/includes/guestNav.php
@@ -10,7 +10,7 @@
         <a href="/Galle-District-mis/" <?php if($currentPage=='guest_home') echo "class=active"?>>Home</a>
         <a href="projects" <?php if($currentPage=='') echo "class=active"?>>Projects</a>
         <a href="annual_reports" <?php if($currentPage=='annual_reports') echo "class=active"?>>Reports</a>
-        <a href="contractors" <?php if($currentPage=='#') echo "class=active"?>>Contractors</a>
+        <a href="contractors" <?php if($currentPage=='contractors') echo "class=active"?>>Contractors</a>
         <a href="about_us" <?php if($currentPage=='about_us') echo "class=active"?>>About Us</a>
         <a href="javascript:void(0);" class="icon" onclick="addResponsiveness()"><i class="fa fa-bars"></i></a>
 </div>
