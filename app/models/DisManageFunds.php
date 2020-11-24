5f39298844974f65a9160f8d4e93941bfc57fdde
sasinduly <2018is045@stu.ucsc.cmb.ac.lk>
2020-11-24 16:13:51 +0530

    still checking

--- app/models/DisManageFunds.php
+++ app/models/DisManageFunds.php
@@ -2,30 +2,36 @@
     namespace model;
     require_once __DIR__ . '/../database/Database.php';
 
-    function getAgencyByID($agency_id){
-        $query = "SELECT * FROM funds WHERE agency_id = ?";
-        $paramType = "i";
-        $paramValue = array($agency_id);
+    class DisManageFunds{
+        private $db_handle;
 
-        $result = $this->db_handle->run_Query($query, $paramType, $paramValue);
-        return $result;
-    }
+        function __construct(){
+            $this->db_handle = new Database();
+        }
 
-    function editAgency($agency_name, $funds_received, $date, $num_of_projects, $total_expense, $total_balance, $agency_id){
-        $query = "UPDATE funds SET agency_name = ?, funds_received = ?, date = ?, num_of_projects = ?, total_expense = ?, total_balance = ? WHERE agency_id = ?";
-        $paramType = "sisiiii";
-        $paramValue = array(
-            $agency_name,
-            $funds_received,
-            $date,
-            $num_of_projects,
-            $total_expense,
-            $total_balance,
-            $agency_id
-        );
+        function getAgencyByID($agency_id){
+            $query = "SELECT * FROM funds WHERE agency_id = ?";
+            $paramType = "i";
+            $paramValue = array($agency_id);
 
-        $this->db_handle->update($query, $paramType, $paramValue);
-    }
+            $result = $this->db_handle->run_Query($query, $paramType, $paramValue);
+            return $result;
+        }
 
- 
+        function editAgency($agency_name, $funds_received, $date, $num_of_projects, $total_expense, $total_balance, $agency_id){
+            $query = "UPDATE funds SET agency_name = ?, funds_received = ?, date = ?, num_of_projects = ?, total_expense = ?, total_balance = ? WHERE agency_id = ?";
+            $paramType = "sisiiii";
+            $paramValue = array(
+                $agency_name,
+                $funds_received,
+                $date,
+                $num_of_projects,
+                $total_expense,
+                $total_balance,
+                $agency_id
+            );
+
+            $this->db_handle->update($query, $paramType, $paramValue);
+        }
+    } 
 ?>
