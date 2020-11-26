59b7651172cbcc31a116177a8a09a1249fe07e33
sasinduly <2018is045@stu.ucsc.cmb.ac.lk>
2020-11-26 23:52:13 +0530

    dis edit funds record

--- app/views/district/dis_edit_funds_record.php
+++ app/views/district/dis_edit_funds_record.php
@@ -21,8 +21,29 @@
                 </div>
                 <div>
                     <label for="funds_received">Funds Received</label>
-                    <input type="text" name="funds_received" id="funds_received" required value="<?php echo $result[0]["funds_recived"]; ?>">
+                    <input type="text" name="funds_received" id="funds_received" required
+                     value="<?php echo $result[0]["funds_recived"]; ?>">
                 </div>
+                <div>
+                    <label for="date">Date</label>
+                    <input type="text" name="date" id="date" required value="<?php echo $result[0]["date"]; ?>">
+                </div>
+                <div>
+                    <label for="num_of_projects">Number of projects</label>
+                    <input type="text" name="num_of_projects" id="num_of_projects" required value="<?php echo $result[0]["num_of_projects"]; ?>">
+                </div>
+                <div>
+                    <label for="$total_expense">Total expense</label>
+                    <input type="text" name="total_expense" id="total_expense" required value="<?php echo $result[0]["total_expense"]; ?>">
+                </div>
+                <div>
+                    <label for="total_balance">Total balance</label>
+                    <input type="text" name="total_balance" id="total_balance" required value="<?php echo $result[0]["total_balance"]; ?>">
+                </div>
+                <div>
+                    <input type="submit" name="save" value="Save">
+                </div>
+            </form>
         </div>   
     </div>
     <?php require_once('includes/footer.php');?>
