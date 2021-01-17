f836e2394fcc99921fe11adb5e92ec7a12a53b52
Sandeepa Ranathunga <sandeeparanatungavcg@gmail.com>
2021-01-18 01:26:38 +0530

    password reset reusable token issue fixed

--- public/js/signup_validation.js
+++ public/js/signup_validation.js
@@ -1,68 +1,59 @@
-function signupValidate(){
-    let password=document.getElementById('password').value;
-    let confirm_password=document.getElementById('confirm_password').value;
-    let email=document.getElementById('email').value;
-    console.log(email);
+function signupValidate() {
+  let email = document.getElementById('email').value;
+  console.log(email);
 
-    if(password!=confirm_password){
-        alert("Password and confirm password does not match!");
-        return false;
-    }
-    if(!validateEmail(email)){
-        alert("Email format is not valid!");
-        return false;
-    }
-    if(emailExist(email)){
-        alert("Email already exist!");
-        return false;
-    }
-    return true;
+  if (!validateEmail(email)) {
+    alert('Email format is not valid!');
+    return false;
+  }
+  if (emailExist(email)) {
+    alert('Email already exist!');
+    return false;
+  }
+  return true;
 }
-function checkPasswordPattern(){
-    let password=document.getElementById('password').value;
-    if(password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/) && password!=''){
-        document.getElementById("pattern").innerHTML="<i class='fas fa-check'></i>";
-        document.getElementById("pattern").style.color="green";
-
-    }
-    else if(password=='')
-        document.getElementById("pattern").innerHTML="";
-
-    else{
-        document.getElementById("pattern").innerHTML="Invalid password pattern";
-        document.getElementById("pattern").style.color="red";
-    }
-
+function checkPasswordPattern() {
+  let password = document.getElementById('password').value;
+  if (
+    password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/) &&
+    password != ''
+  ) {
+    document.getElementById('pattern').innerHTML =
+      "<i class='fas fa-check'></i>";
+    document.getElementById('pattern').style.color = 'green';
+  } else if (password == '') document.getElementById('pattern').innerHTML = '';
+  else {
+    document.getElementById('pattern').innerHTML = 'Invalid password pattern';
+    document.getElementById('pattern').style.color = 'red';
+  }
 }
 
-function confirmPassword(){
-    let password=document.getElementById('password').value;
-    let confirm_password=document.getElementById('confirm_password').value;
-    if(password==confirm_password && password!=''){
-        document.getElementById("confirm_warning").innerHTML="<i class='fas fa-check'></i>";
-        document.getElementById("confirm_warning").style.color="green";
-    }
-    else if(confirm_password=='')
-        document.getElementById("confirm_warning").innerHTML="";
-    else{
-        document.getElementById( "confirm_warning").innerHTML="<i class='fas fa-times'></i>";
-        document.getElementById("confirm_warning").style.color="red";
-    }
-
-
+function confirmPassword() {
+  let password = document.getElementById('password').value;
+  let confirm_password = document.getElementById('confirm_password').value;
+  if (password == confirm_password && password != '') {
+    document.getElementById('confirm_warning').innerHTML =
+      "<i class='fas fa-check'></i>";
+    document.getElementById('confirm_warning').style.color = 'green';
+  } else if (confirm_password == '')
+    document.getElementById('confirm_warning').innerHTML = '';
+  else {
+    document.getElementById('confirm_warning').innerHTML =
+      "<i class='fas fa-times'></i>";
+    document.getElementById('confirm_warning').style.color = 'red';
+  }
 }
-function validateEmail(email){
-    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(String(email).toLowerCase());
+function validateEmail(email) {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toLowerCase());
 }
-function setUserID(){
-    let prefix=document.getElementById('office_id').value;
-    document.getElementById("user_id").value=prefix+getRandomDigits();
-    
+function setUserID() {
+  let prefix = document.getElementById('office_id').value;
+  document.getElementById('user_id').value = prefix + getRandomDigits();
 }
-function emailExist($email){
-    console.log($emails);
+function emailExist($email) {
+  console.log($emails);
 }
-function getRandomDigits(){
-    return Math.floor(Math.random() * (99-10))+10;
+function getRandomDigits() {
+  return Math.floor(Math.random() * (99 - 10)) + 10;
 }
