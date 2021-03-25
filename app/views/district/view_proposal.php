0e544dff3dc2d715b3574512c94c3e3680c2a4b8
Sandeepa Ranathunga <sandeeparanatungavcg@gmail.com>
2021-03-26 03:11:14 +0530

    custom alert added

--- app/views/district/view_proposal.php
+++ app/views/district/view_proposal.php
@@ -10,6 +10,7 @@
     <?php include 'includes/cssLinks.php';?>
     <link rel="stylesheet" href="css/userPage.css">
     <link rel="stylesheet" href="css/proposalPage.css">
+    <link rel="stylesheet" href="css/alertBox.css">
     <link rel="icon" href="images/logo.png">
     <title>Document</title>
 </head>
@@ -20,6 +21,7 @@
         <h2>Project Proposal</h2>
     </div>
     <div class="container proposal_container">
+        
         <div class="detail_box">
             <div class="content_line">
                 <label for="Divisional Secretariat">Divisional Secretariat : </label>
@@ -45,10 +47,14 @@
             <h4>Click the icon to download the proposal</h4>    
         </div>
         <div class="evaluation_box" style="<?php echo ($data[6]=='pending') ? 'display:flex' : 'display:none' ?>">
-            <form action="" method="post">
-                <button name="approve" class="approve" type="submit">Approve</button>
-                <button name="reject" class="reject" type="submit">Reject</button>
+            <form action="" method="post" id="approve_form">
+                <input type="hidden" name="approve" value="approve">
             </form>
+            <form action="" method="post" id="reject_form">
+                <input type="hidden" name="reject" value="reject">
+            </form>
+            <button name="approve" class="approve">Approve</button>
+            <button name="reject" class="reject">Reject</button>
         </div>
         <div class="status_box" style="<?php echo ($data[6]=='pending') ? 'display:none' : 'display:flex' ?>">
             <div class="state" style="<?php echo ($data[6]=='approved') ? 'display:flex' : 'display:none' ?>">
@@ -68,6 +74,15 @@
             
         </div>
     </div>
+    <div class="popup_box">
+        <i class="fas fa-exclamation"></i>
+        <h1>Are you sure you want to proceed!</h1>
+        <div class="btns">
+            <button class="btn1">Proceed</button>
+            <button class="btn2">Cancel</button>
+        </div>
+    </div>
     <?php require_once('includes/footer.php');?>
+    <script src="js/approval.js"></script>
 </body>
 </html>
---
+++ public/includes/alert.php
@@ -0,0 +1,9 @@
+<div class="popup_box">
+    <i class="fas fa-exclamation"></i>
+    <h1>Your account will be deleted Permanently!</h1>
+    <label>Are you sure to proceed?</label>
+    <div class="btns">
+        <a href="#" class="btn1">Cancel Process</a>
+        <a href="#" class="btn2">Delete Account</a>
+    </div>
+</div>
