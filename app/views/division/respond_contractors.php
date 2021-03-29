45327f352191b060f65e4c7c5ccfad3517c2662f
sasinduly <2018is045@stu.ucsc.cmb.ac.lk>
2021-03-29 10:54:07 +0530

    minoer changes

--- app/views/division/respond_contractors.php
+++ app/views/division/respond_contractors.php
@@ -18,26 +18,7 @@
         <h2>Respond to contractor requests</h2>
     </div>
     <div class="container">
-        <div class="combutton">
-            <div class="flex-container" >
-                <div style="width:75%"><h2 style="font-size:17px"><br>Contractor request001</h2></div>
-                <div style="width:15%"><button>View</button></div>
-                <!-- <div style="width:15%"><button>Accept</button></div>
-                <div style="width:15%"><button>Delete</button></div> -->
-            </div>
-            <div class="flex-container" >
-                <div style="width:75%"><h2 style="font-size:17px"><br>Contractor request002</h2></div>
-                <div style="width:15%"><button>View</button></div>
-                <!-- <div style="width:15%"><button>Accept</button></div>
-                <div style="width:15%"><button>Delete</button></div> -->
-            </div>
-            <div class="flex-container" >
-                <div style="width:75%"><h2 style="font-size:17px"><br>Contractor request003</h2></div>
-                <div style="width:15%"><button>View</button></div>
-                <!-- <div style="width:15%"><button>Accept</button></div>
-                <div style="width:15%"><button>Delete</button></div> -->
-            </div>        
-        </div>
+
     </div>
     <?php require_once('includes/footer.php');?>
 </body>
